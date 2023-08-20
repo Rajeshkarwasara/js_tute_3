8213a7621a9bab7204bbe9ca89b4728e2ce1369d
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 10:12:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let y = "'hello'"
+let x = "hello 'java'"
+console.log(y, x);
+
+
+// next
+// let l=y.length
+console.log(y.length, x.length);
+alert("hello")
+
+
+// next 
+let message1 = "hello rajesh how are you";
+let msg = message1.slice(6, 13);
+console.log(msg);
+
+
+let msg2 = message1.substring(-6,);
+console.log(msg2);
+
+
+let msg3 = message1.substr(-6,);
+console.log(msg3);
